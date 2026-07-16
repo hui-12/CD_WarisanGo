d4b68eec10d75697024d729330912441937fe596
hui-12 <laiyh091204@gmail.com>
2026-07-16 18:56:20 +0800

    basic setup

---
+++ warisango/src/main/java/com/warisango/WarisangoApplication.java
@@ -0,0 +1,14 @@
+package com.warisango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class WarisangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarisangoApplication.class, args);
+	}
+
+}
---
+++ warisango/src/test/java/com/warisango/WarisangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warisango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarisangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
