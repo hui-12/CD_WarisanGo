11dbfffca1ba67310548f83f1cc26af5f8aa7809
TianYou6605 <taytianyou6605@gmail.com>
2026-08-07 02:49:35 +0800

    updated 5 page (filename error)

--- src/main/java/com/warisango/controller/HomeController.java
+++ src/main/java/com/warisango/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "admin/AIDiscoveryPage.html";
+        return "resources/view/CheckInPage.html";
     }
 }
