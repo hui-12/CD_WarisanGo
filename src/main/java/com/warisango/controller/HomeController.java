6a18a83e319e6faee35066e7e5419f2e27ca623d
Chin Zheng Hao <chinzhenghao2@gmail.com>
2026-08-07 08:51:55 +0800

    Add BusinessDirectoryController and business-directory.html for business directory feature

--- src/main/java/com/warisango/controller/HomeController.java
+++ src/main/java/com/warisango/controller/HomeController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // Serve the business directory as the application root page
     @GetMapping({"/", "/business-directory"})
     public String home() {
-        // Thymeleaf template located at src/main/resources/templates/view/business-directory.html
-        return "view/business-directory";
+        return "business-directory";
     }
 }
