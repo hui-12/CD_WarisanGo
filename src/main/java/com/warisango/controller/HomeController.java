bb7e8553efbebc7454ccf34ba5a9ac45ed7522b6
TianYou6605 <taytianyou6605@gmail.com>
2026-08-07 09:25:42 +0800

    reward module ui and simple nav

--- src/main/java/com/warisango/controller/HomeController.java
+++ src/main/java/com/warisango/controller/HomeController.java
@@ -6,8 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "admin/AdminHomePage.html";
+    @GetMapping({"/", "/CheckInPage"})
+    public String checkIn() {
+        return "CheckInPage";
+    }
+
+    @GetMapping("/BadgePage")
+    public String badges() {
+        return "BadgePage";
+    }
+
+    @GetMapping("/RewardPage")
+    public String rewards() {
+        return "RewardPage";
+    }
+
+    @GetMapping("/ChallengePage")
+    public String challenges() {
+        return "ChallengePage";
+    }
+
+    @GetMapping("/AdminChallengePage")
+    public String adminChallenges() {
+        return "admin/AdminChallengePage";
     }
 }
