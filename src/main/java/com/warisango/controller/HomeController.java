cf53b764a46ac7634af3c67cca9303f17e96a493
TianYou6605 <taytianyou6605@gmail.com>
2026-08-07 08:32:52 +0800

    controller

--- src/main/java/com/warisango/controller/HomeController.java
+++ src/main/java/com/warisango/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "resources/view/CheckInPage.html";
+        return "admin/AdminHomePage.html";
     }
 }
--- src/main/java/com/warisango/controller/RewardController.java
+++ src/main/java/com/warisango/controller/RewardController.java
@@ -1,1 +1,13 @@
-// TODO
+package com.warisango.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RewardController {
+
+    @GetMapping("/")
+    public String home() {
+        return "resources/view/CheckInPage.html";
+    }
+}
