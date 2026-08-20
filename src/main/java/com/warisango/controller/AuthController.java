6de1282bf7f9a870c4ab7cef032af221aacd4c66
foodycookie <onghao.howard@gmail.com>
2026-08-20 23:09:08 +0800

    create a placeholder login paage

--- src/main/java/com/warisango/controller/AuthController.java
+++ src/main/java/com/warisango/controller/AuthController.java
@@ -1,1 +1,13 @@
-// TODO
+package com.warisango.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "LoginPage";
+    }
+}
